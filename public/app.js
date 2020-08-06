3b82fc5ae161458d3bf511473444f92ebc1a6705
Joel Elliston <lachre@outlook.com>
2020-08-06 19:00:40 +0300

    got it working but still buggy when game over

--- public/app.js
+++ public/app.js
@@ -35,91 +35,92 @@ const nickNamesDictionary = [
   "raven nightjar",
   "sepia barbet",
 ];
-
 let obstacleTimers = [];
 let gameStarted = false;
 let gameTimerId;
 let myScore = 0;
 let highScore = 0;
 let highScoreNickname = "anonymous panda";
-let myNickname; 
+let myNickname;
 
 if (localStorage.getItem("flappy-nickname")) {
-    myNickname = localStorage.getItem("flappy-nickname");
-  } else {
-    myNickname = nickNamesDictionary[Math.floor(Math.random() * 34)];
-    localStorage.setItem("flappy-nickname", myNickname);
+  myNickname = localStorage.getItem("flappy-nickname");
+} else {
+  myNickname = nickNamesDictionary[Math.floor(Math.random() * 34)];
+  localStorage.setItem("flappy-nickname", myNickname);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bird = document.querySelector(".bird");
+  const gameDisplay = document.querySelector(".game-container");
+  const ground = document.querySelector(".ground-moving");
+  let nicknameInput = document.getElementById("nickname-input");
+  let updateNicknameBtn = document.getElementById("update-nickname");
+  let scoreLabel = document.getElementById("score-label");
+  let topScoreLabel = document.getElementById("top-label");
+  let scoreList = document.getElementById("score-list");
+
+  let birdLeft = 220;
+  let birdBottom = 350;
+  let gravity = 2;
+  let isGameOver = false;
+  let gap = 450;
+
+  const filterNickname = async (nicknameText) => {
+    const http = new XMLHttpRequest();
+    let encodedText = encodeURIComponent(nicknameText);
+    http.open("GET", profanityBaseURL + encodedText + "&fill_text=***");
+    http.send();
+    http.onload = () => {
+      myNickname = http.responseText;
+      nicknameInput.value = myNickname;
+      localStorage.setItem("flappy-nickname", myNickname);
+    };
+  };
+
+  topScoreLabel.innerHTML =
+    "Top score - " + highScore + "pts by " + highScoreNickname;
+  nicknameInput.value = myNickname;
+  updateNicknameBtn.addEventListener("click", () => {
+    filterNickname(nicknameInput.value);
+  });
+
+  window.addEventListener("keydown", function (e) {
+    if (e.keyCode == 32 && e.target == document.body) {
+      e.preventDefault();
+    }
+  });
+
+  gameDisplay.onclick = function () {
+    if (!gameStarted) {
+      gameStarted = true;
+      document.addEventListener("keydown", control);
+      gameTimerId = setInterval(startGame, 20);
+      generateObstacles();
+    }
+  };
+
+  function startGame() {
+    birdBottom -= gravity;
+    bird.style.bottom = birdBottom + "px";
+    bird.style.left = birdLeft + "px";
   }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const bird = document.querySelector('.bird');
-    const gameDisplay = document.querySelector('.game-container');
-    const ground = document.querySelector(".ground-moving");
-    let nicknameInput = document.getElementById("nickname-input");
-    let updateNicknameBtn = document.getElementById("update-nickname");
-    let scoreLabel = document.getElementById("score-label");
-    let scoreList = document.getElementById("score-list");
-
-    let birdLeft = 220;
-    let birdBottom = 350;
-    let gravity = 2;
-    let isGameOver = false;
-    let gap = 450;
-
-    const filterNickname = async (nicknameText) => {
-        const http = new XMLHttpRequest();
-        let encodedText = encodeURIComponent(nicknameText);
-        http.open("GET", profanityBaseURL + encodedText + "&fill_text=***");
-        http.send();
-        http.onload = () => {
-            myNickname = http.responseText;
-            nicknameInput.value = myNickname;
-            localStorage.setItem("flappy-nickname", myNickname);
-        };
-    };
-
-    topScoreLabel.innerHTML = "Top score - " + highScore + "pts by " + highScoreNickname;
-    nicknameInput.value = myNickname;
-    updateNicknameBtn.addEventListener("click", () => {
-        filterNickname(nicknameInput.value);
-    });
-
-    // this prevents the default behavior of using the spacebar (scrolling down)
-    window.addEventListener("keydown", function (e) => {
-        if(e.keyCode == 32 && e.target == document.body) {
-            e.preventDefault();
-        }
-    });
-
-    gameDisplay.onclick = function () {
-        if (!gameStarted) {
-            gameStarted = true;
-            document.addEventListener("keydown", control);
-            gameTimerId = setInterval(startGame, 20)
-        }    
-    };
-
-    function startGame() {
-        birdBottom -= gravity
-        bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
+  function control(e) {
+    if (e.keyCode === 32 && !isGameOver) {
+      jump();
     }
+  }
 
-    function control(e) {
-        if (e.keyCode === 32 && !isGameOver) {
-            jump();
-        }
-    };
-
-    function jump() {
-        if (birdBottom < 500) birdBottom += 50;
-        bird.style.bottom  = birdBottom + 'px';
-    }
+  function jump() {
+    if (birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + "px";
+  }
 
   function generateObstacles() {
     if (!isGameOver) {
       let obstacleLeft = 500;
-      let obstacleBottom = Math.random() + 60;
+      let obstacleBottom = Math.random() * 60;
 
       const obstacle = document.createElement("div");
       const topObstacle = document.createElement("div");
@@ -165,14 +166,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-    function gameOver() {
-        scoreLabel.innerHTML += " | Game Over";
-        clearInterval(gameTimerId);
-        console.log('game over');
-        isGameOver = true;
-        document.removeEventListener("keydown", control);
-        ground.classList.add("ground");
-        ground.classList.remove("ground-moving");
-    }
-
-})
+  function gameOver() {
+    scoreLabel.innerHTML += " | Game Over";
+    clearInterval(gameTimerId);
+    isGameOver = true;
+    document.removeEventListener("keydown", control);
+    ground.classList.add("ground");
+    ground.classList.remove("ground-moving");
+  }
+});
